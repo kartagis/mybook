5b85467ee1811d684198de5e586eb5eab5b6b1bc
Tolga Ozses <tolga@ozses.net>
2017-04-07 23:42:57 +0300

    WIP

--- www/js/gallery.js
+++ www/js/gallery.js
@@ -1,3 +1,5 @@
+var arr = {};
+var fd = new FormData();
 document.addEventListener("deviceready", function() {
   cordova.plugins.photoLibrary.getLibrary(function(res) {
     var library = res.library;
@@ -6,24 +8,25 @@ document.addEventListener("deviceready", function() {
       var id = libraryItem.id.split(";")[0];
       images += "<div class='imgHolder'><img class='img' height='100' width='100' src='"+libraryItem.thumbnailURL+"' data-id='"+id+"' style='margin: 5px'/></div>";
       cordova.plugins.photoLibrary.getPhoto(libraryItem, function(fullPhotoBlob) {
-        arr = [];
-        arr.push(id, fullPhotoBlob);
-        /*
-        var fd = new FormData();
-        var xhr = new XMLHttpRequest();
-        fd.append("bookuser",2);
-        fd.append("image",fullPhotoBlob, "image"+new Date().getTime()+".jpg");
-        xhr.open("POST", "http://iothook.com:8000/api/v1/books/");
-        xhr.send(fd);
-        */
+        id = libraryItem.id.split(";")[0];
+        arr[id] = fullPhotoBlob;
         console.log(arr);
       })
     })
     $("#images").html(images);
   })
 })
-$("#images").on("click", ".imgHolder", function() {
+$("#images").on("click", ".imgHolder", function(arr) {
   $(this).toggleClass("checked");
-  var imgSRC = $(this).find('img').data('blob');
+  var id = $(this).find('img').data('id');
+  var blob = arr[id];
+  fd.append("bookuser",2);
+  fd.append("image",arr["'+id'+"],"image"+new Date().getTime()+".jpg");
   $("#submit").css("display","block");
+})
+$("#submit").on("click", function(fd) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://iothook.com:8000/api/v1/books/");
+  xhr.send(fd);
+  console.log(arr);
 })
