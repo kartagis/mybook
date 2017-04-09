6623773a19afc948a2a066f1bfe3c0bcb6fee374
Tolga Ozses <tolga@ozses.net>
2017-04-09 20:47:14 +0300

    Uploading finally works

--- www/js/gallery.js
+++ www/js/gallery.js
@@ -1,7 +1,11 @@
-var arr = {};
+var init = {};
+var sent = [];
+var id;
 var fd = new FormData();
 document.addEventListener("deviceready", function() {
+  $("#submit").css("display","none");
   cordova.plugins.photoLibrary.getLibrary(function(res) {
+    ActivityIndicator.show("Lütfen bekleyin, kütüphane yükleniyor");
     var library = res.library;
     var images = '';
     library.forEach(function(libraryItem) {
@@ -9,24 +13,27 @@ document.addEventListener("deviceready", function() {
       images += "<div class='imgHolder'><img class='img' height='100' width='100' src='"+libraryItem.thumbnailURL+"' data-id='"+id+"' style='margin: 5px'/></div>";
       cordova.plugins.photoLibrary.getPhoto(libraryItem, function(fullPhotoBlob) {
         id = libraryItem.id.split(";")[0];
-        arr[id] = fullPhotoBlob;
-        console.log(arr);
+        init[id] = fullPhotoBlob;
       })
     })
+    ActivityIndicator.hide();
     $("#images").html(images);
   })
 })
-$("#images").on("click", ".imgHolder", function(arr) {
+$("#images").on("click", ".imgHolder", function() {
   $(this).toggleClass("checked");
-  var id = $(this).find('img').data('id');
-  var blob = arr[id];
-  fd.append("bookuser",2);
-  fd.append("image",arr["'+id'+"],"image"+new Date().getTime()+".jpg");
+  id = $(this).find('img').data('id');
+  sent.push(init[id]);
   $("#submit").css("display","block");
 })
-$("#submit").on("click", function(fd) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://iothook.com:8000/api/v1/books/");
-  xhr.send(fd);
-  console.log(arr);
+$("#submit").on("click", function() {
+  sent.forEach(function(blob) {
+    console.log(blob);
+    var fd = new FormData();
+    var xhr = new XMLHttpRequest();
+    fd.append("bookuser",2);
+    fd.append("image",blob,"image"+Math.random().toString().split(".")[1]+".jpg");
+    xhr.open("POST", "http://iothook.com:8000/api/v1/books/");
+    xhr.send(fd);
+  })
 })
